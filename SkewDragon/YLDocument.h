b37d18ed3dbc0382d7f4dbd726e8c7b7ab0fdbe3
Yung-Luen Lan <yllan@me.com>
2014-01-14 22:33:26 +0800

    Initial Commit

---
+++ SkewDragon/YLDocument.h
@@ -0,0 +1,13 @@
+//
+//  YLDocument.h
+//  SkewDragon
+//
+//  Created by Yung-Luen Lan on 1/14/14.
+//  Copyright (c) 2014 Yung-Luen Lan. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface YLDocument : NSDocument
+
+@end
---
+++ SkewDragon/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  SkewDragon
+//
+//  Created by Yung-Luen Lan on 1/14/14.
+//  Copyright (c) 2014 Yung-Luen Lan. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
